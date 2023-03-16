239b22d1602f03bac702fae43d63aeedc82094dd
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2023-03-16 22:20:26 +0300

    Lab completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    private
+    def clickbait
+        unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.match?(/Top \d+/) || title.include?("Guess"))
+            errors.add(:title, "must be clickbait-y")
+        end
+    end
+    
 end
